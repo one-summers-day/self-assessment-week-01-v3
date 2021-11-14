1d00ace04cb6994e4589b3728ee999a4251b42e0
ericjlie <ericjlie@gmail.com>
2021-11-13 16:59:45 -0800

    Final commit

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -43,27 +43,6 @@ Tree.prototype.addChild = function(value) {
   this.children.push(child);
 }
 
-// Tree.prototype.map = function(cb) {
-//   //instantiate new Tree
-//   var newTree = new Tree();
-//   //perform cb function on current node and set to newTree val
-//   newTree.value = cb(this.value);
-//   //recurse through old tree children
-//   const treeRecurse = function(cb, node) {
-//     let value = cb(node.value);
-//     newTree.addChild(value);
-//     for (let child in node.children) {
-//       treeRecurse(cb, child);
-//     }
-//   }
-//   //Initiate recursion by going through each of this.node's children
-//   //There's definitely a more elegant way of doing this
-//   for (let i = 0; i < this.children.length; i++) {
-//     treeRecurse(cb, this.children[i]);
-//   }
-//   return newTree;
-// }
-
 Tree.prototype.map = function(cb) {
   //instantiate new Tree
   var newTree = new Tree();
@@ -80,8 +59,8 @@ Tree.prototype.map = function(cb) {
   }
   //Initiate recursion by going through each of this.node's children
   //There's definitely a more elegant way of doing this
-  this.children.forEach(()=> {
-    treeRecurse(cb, this.children[i]);
+  this.children.forEach((child)=> {
+    treeRecurse(cb, child);
   })
   return newTree;
 }

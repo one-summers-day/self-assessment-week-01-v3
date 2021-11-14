dca53356baf1625c72118da0d1d50fce97a49988
one-summers-day <jiro.horikoshi1@gmail.com>
2021-11-13 19:34:45 -0600

    Need to debug recursion

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -28,10 +28,52 @@
   *  root1.value // still 1
   */
 
+//Step 1:
 var Tree = function(value) {
   this.value = value;
+  //Step 1.1: Make a children array keeping track of all the children originating from a particular element
   this.children = [];
+
+  //Step 1.2: Make an array of all tree elements so I don't have to use recursion. XD
+  this.family = [this];
+
+  return this;
+
 };
 
+//Step 2: Implement a addChild() function which allows for the tree to grow
+Tree.prototype.addChild = function(value) {
+  //Step 2.1: Instantiate a new child
+  let child = new Tree(value);
 
+  //Step 2.2: Update family and children for this and pass family onto child (all tree nodes have the same family property)
+  this.family.push(child);
+  this.children.push(child);
+  child.family = this.family;
 
+  return child;
+};
+
+//Step 3:
+Tree.prototype.map = function(myAwesomeFunction) {
+  //Step 3.1: Instantiate a new tree that's identical to the one we have
+  let myAwesomeReturnValue = new Tree(0);
+  myAwesomeReturnValue.value = this.value;
+  myAwesomeReturnValue.children = this.children;
+  myAwesomeReturnValue.family = this.family;
+
+  //Step 3.2: Use some recursion to run myAwesomeFunction through all of the children and all of the family.
+  function revengeOftheRecursion(treeElement) {
+    if (treeElement.children !== []){
+      for (let i = 0; i < treeElement.children.length; i++) {
+        revengeOftheRecursion(treeElement.children[i])
+      }
+    }
+    else {
+      treeElement.value = myAwesomeFunction(treeElement.value);
+    }
+  }
+  return myAwesomeReturnValue.children[0];
+}
+
+let myTree = new Tree(1);

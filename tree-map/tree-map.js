c7769e4eb94f3223f9c8252129a8d89b5f65a716
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-11-13 16:50:29 -0800

    Added comment to the methods

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -28,15 +28,24 @@
   *  root1.value // still 1
   */
 
-var Tree = function(value) {
+
+/**
+ * Tree object constructor
+ * @param {number} value - tree instance value
+ */
+const Tree = function(value) {
   this.value = value;
   this.children = [];
 };
 
-// add method addChild for the Tree class
+/**
+ * add a child node to the current Tree (this)
+ * @param {number} value - value
+ * @returns {object - Instance of Tree} - returns the newSubTree added to this Tree
+ */
 Tree.prototype.addChild = function(value) {
   // create new subTree with the passed in value
-  var newSubTree = new Tree(value);
+  const newSubTree = new Tree(value);
   // push new subTree to this tree instance children
   this.children.push(newSubTree);
   // return newSubTree
@@ -46,10 +55,11 @@ Tree.prototype.addChild = function(value) {
 // add map method
 Tree.prototype.map = function(cb) {
   // create new Tree and add the first subTree
-  var newTree = new Tree(cb(this.value));
+  const newTree = new Tree(cb(this.value));
   // add new tree children recursively calling map on each children
-  for (var i = 0; i < this.children.length; i++) {
+  for (let i = 0; i < this.children.length; i++) {
     // newTree.chidren[i] will only be assigned a value once treeChildren.map(cb) finishes executing
+    const treeChildren = this.children[i];
     newTree.children[i] = treeChildren.map(cb);
   }
   // return the new tree
@@ -59,21 +69,23 @@ Tree.prototype.map = function(cb) {
 
 
 // tests
-var root1 = new Tree(1);
-  var branch2 = root1.addChild(2);
-  var branch3 = root1.addChild(3);
-  var leaf4 = branch2.addChild(4);
-  var leaf5 = branch2.addChild(5);
-  var leaf6 = branch3.addChild(6);
-  var leaf7 = branch3.addChild(7);
+const root1 = new Tree(1);
+const branch2 = root1.addChild(2);
+const branch3 = root1.addChild(3);
+const leaf4 = branch2.addChild(4);
+const leaf5 = branch2.addChild(5);
+const leaf6 = branch3.addChild(6);
+const leaf7 = branch3.addChild(7);
 
-var newTree = root1.map(function (value) {
+const newTree = root1.map(function (value) {
   return value * 2;
 });
 
-newTree.value // 2
-newTree.children[0].value // 4
-newTree.children[1].value // 6
-newTree.children[0].children[1].value // 10
-newTree.children[1].children[1].value // 14
-root1.value // still 1
+console.log(newTree)
+
+console.log(newTree.value); // 2
+console.log(newTree.children[0].value); // 4
+console.log(newTree.children[1].value); // 6
+console.log(newTree.children[0].children[1].value); // 10
+console.log(newTree.children[1].children[1].value); // 14
+console.log(root1.value); // still 1

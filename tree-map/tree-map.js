f9044a1628ac9a5c6a11f7ad43d5c64a04ee90dd
bgtdinh <bgtdinh@gmail.com>
2021-11-13 17:23:01 -0800

    Create pseudoclassical Tree

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -31,7 +31,54 @@
 var Tree = function(value) {
   this.value = value;
   this.children = [];
+  this.parent;
 };
+
+Tree.prototype.map = function(mappingFunction) {
+  var newTree;
+  //take in function
+  //create new tree class
+  //return out new tree with changed values
+  mapRootAndBranches(tree) {
+    //base case run callback function on value in tree
+    var currentTree = new Tree(mappingFunction(tree.value));
+    if(tree.parent === undefined) {
+      newTree = currentTree;
+    }
+    if(tree.parent) {
+      var branchOfNewTree = newTree.addChild(1);
+      branchOfNewTree = currentTree;
+    }
+
+    //need some way of traversing into the tree while also making new branches
+    //not sure exactly how to do it
+
+    for(var i = 0; i < tree.children.length; i++) {
+      ; //must add new branch for tree if children exist
+
+      mapRootAndBranches(tree.children[i]);
+    }
+
+    //recurse through children and call function on each
+    //mapRootAndBranch must create new tree not sure how to do with helper
+
+  }
+
+
+  //recurses through old tree and run function on each value in branches
+  mapRootAndBranches(this);
+
+  return newTree;
+
+};
+
+Tree.prototype.addChild = function(numberOfBranches) {
+  for(var i = 0; i < numberOfBranches; i++) {
+    var currentBranch = new Tree(Math.floor(Math.random() * 10));
+    currentBranch.parent = this;
+    this.children.push(currentBranch);
+  }
+}
 
 
 

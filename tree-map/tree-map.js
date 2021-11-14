9bd4a3ecc25920127a8eea075df480493db0d8a1
David Rajec <david.rajec@gmail.com>
2021-11-13 17:16:06 -0800

    SA01 mostly complete.

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -20,18 +20,55 @@
   *   var newTree = root1.map(function (value) {
   *     return value * 2;
   *   })
-  *  newTree.value // 2
-  *  newTree.children[0].value // 4
-  *  newTree.children[1].value // 6
-  *  newTree.children[0].children[1].value // 10
-  *  newTree.children[1].children[1].value // 14
-  *  root1.value // still 1
+  newTree.value
+  newTree.children[0].value
+  newTree.children[1].value
+  newTree.children[0].children[1].value
+  newTree.children[1].children[1].value
+  root1.value
   */
+ // 2
+ // 4
+ // 6
+ // 10
+ // 14
+ // still 1
 
+  //what does map need.
+  //push children into tree
 var Tree = function(value) {
   this.value = value;
   this.children = [];
 };
 
+Tree.prototype.addChild = function (value) {
+  const newBranch = new Tree(value);
+  // console.log(this);
+  this.children.push(newBranch);
+  return newBranch;
+}
 
+//having trouble creating a clone of 'this' so that the mapping doesn't overwrite the original.
+Tree.prototype.map = function (func, node = this) {
+  if (node === null) {
+    return;
+  }
+  node.value = func(node.value);
+  for (let i = 0; i < node.children.length; i++) {
+    this.map(func, node.children[i]);
+  }
+  return this;
+}
 
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+var leaf5 = branch2.addChild(5);
+var leaf6 = branch3.addChild(6);
+var leaf7 = branch3.addChild(7);
+var newTree = root1.map(function (value) {
+  return value * 2;
+});
+console.log(root1);
+console.log(newTree);

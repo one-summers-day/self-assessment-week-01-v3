672fb06ca1d7b074a6f6b5245b80b672a67bdaf3
sleepisgood1 <jae1994krw@gmail.com>
2021-11-13 17:14:58 -0800

    Attempted at map

--- tree-map/tree-map.js
+++ tree-map/tree-map.js
@@ -33,5 +33,52 @@ var Tree = function(value) {
   this.children = [];
 };
 
+Tree.prototype.map = function(fn) {
+  // var newvalue = fn(this.value)
+  // //set new tree's value with that new value from function
+  // var newTree1 = new Tree(newvalue);
+
+  // if this.children has values, run function on this.chidlren and push the reuslt child {} onto newTree.child
+  // //helper funciton
+  var changeValue = function(currentTree) {
+    //run function with the value
+    var newvalue = fn(currentTree.value)
+    //set new tree's value with that new value from function
+    var newTree1 = new Tree(newvalue);
+    //if current tree has any children, run helper function to recurse through
+    if (currentTree.children.length !== 0) {
+      for (var i = 0; i<currentTree.children.length;i++){
+        newTree1.children.push(changeValue(currentTree.children[i]))
+      }
+    }
+    return newTree1
+  }
+  return changeValue(this)
+}
+
+Tree.prototype.addChild = function(value) {
+  this.children.push(new Tree(value));
+}
 
 
+//tests
+var root1 = new Tree(1);
+var branch2 = root1.addChild(2);
+var branch3 = root1.addChild(3);
+var leaf4 = branch2.addChild(4);
+// var leaf5 = branch2.addChild(5);
+// var leaf6 = branch3.addChild(6);
+// var leaf7 = branch3.addChild(7);
+
+var newTree = root1.map(function (value) {
+return value * 2;
+})
+
+console.log(root1.children[0])
+console.log(newTree.value) // 2
+console.log(leaf4.value)
+// console.log(newTree.children[0].value) // 4
+// console.log(newTree.children[1].value) // 6
+// console.log(newTree.children[0].children[1].value) // 10
+// console.log(newTree.children[1].children[1].value) // 14
+// console.log(root1.value) // still 1
